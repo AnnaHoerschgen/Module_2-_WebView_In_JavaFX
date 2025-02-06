ca26214878c5082332ffc70fa0c359f2b9d0e08f
Anna Hoerschgen <ah0959247@otc.edu>
2025-02-06 15:23:44 -0600

    Assignments Complete

--- ModuleTwo.java
+++ ModuleTwo.java
@@ -1,80 +1,94 @@
 /*
  * Name: Anna Hoerschgen
- * Date: Febuary 6, 2025
+ * Date: February 6, 2025
  * Filename: ModuleTwo.java
  * Assignment: Simple Web Browser in JavaFX
  */
 
-// Imports
+// Required imports for JavaFX WebView and other functionalities
 import java.net.URL;
-import java.util.Observable;
+import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebHistory;
+import javafx.scene.web.WebView;
 
 public class ModuleTwo implements Initializable {
+    
     @FXML
-    private WebView webView;
+    private WebView webView; // WebView component to display web pages
 
     @FXML
-    private TextField textField;
+    private TextField textField; // TextField for entering the URL
 
-    private WebEngine engine;
-    private WebHistory history;
+    private WebEngine engine; // WebEngine to load and render web pages
+    private WebHistory history; // WebHistory to track browsing history
 
-    private String homePage;
+    private String homePage; // Default home page URL
+    private double webZoom; // Zoom level for the WebView
 
-    private double webZoom;
-
+    // Initialize method called when the controller is loaded
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-        engine = webView.getEngine();
-        homePage = "www.google.com";
-        textField.setText(homePage);
-        webZoom = 1;
-        loadPage();
+        engine = webView.getEngine(); // Get WebEngine instance from WebView
+        homePage = "www.google.com"; // Set default homepage
+        textField.setText(homePage); // Display homepage in text field
+        webZoom = 1; // Default zoom level
+        loadPage(); // Load the initial homepage
     }
 
+    // Loads the page entered in the text field
     public void loadPage() {
-        engine.load("http://"+textField.getText());
+        engine.load("http://" + textField.getText()); // Load the URL
     }
 
+    // Reloads the current page
     public void refreshPage() {
-        
         engine.reload();
     }
 
+    // Zoom in by increasing zoom level
     public void zoomIn() {
         webZoom += 0.25;
         webView.setZoom(webZoom);
     }
 
+    // Zoom out by decreasing zoom level
     public void zoomOut() {
         webZoom -= 0.25;
         webView.setZoom(webZoom);
     }
 
+    // Displays browsing history in the console
     public void displayHistory() {
-        history = engine.getHistory;
+        history = engine.getHistory(); // Get browsing history
         ObservableList<WebHistory.Entry> entries = history.getEntries();
 
         for (WebHistory.Entry entry : entries) {
-            // System.out.println(entry);
-            System.out.println(entry.getUrl()+" "+entry.getLastVisitedDate());
+            System.out.println(entry.getUrl() + " " + entry.getLastVisitedDate()); // Print history entries
         }
     }
 
+    // Navigates back in browsing history
     public void back() {
-        history = engine.getHistory();
+        history = engine.getHistory(); // Get browsing history
         ObservableList<WebHistory.Entry> entries = history.getEntries();
-        history.go(-1);
-        textField.setText(entries.get(history.getCurrentIndex));
+        history.go(-1); // Go back one page
+        textField.setText(entries.get(history.getCurrentIndex()).getUrl()); // Update text field
     }
 
+    // Navigates forward in browsing history
     public void forward() {
-        history = engine.getHistory();
+        history = engine.getHistory(); // Get browsing history
         ObservableList<WebHistory.Entry> entries = history.getEntries();
-        history.go(1);
-        textField.setText(entries.get(history.getCurrentIndex));
+        history.go(1); // Go forward one page
+        textField.setText(entries.get(history.getCurrentIndex()).getUrl()); // Update text field
     }
 
+    // Executes a JavaScript command to navigate to YouTube
     public void executeJS() {
         engine.executeScript("window.location = \"https://youtube.com\";");
     }
