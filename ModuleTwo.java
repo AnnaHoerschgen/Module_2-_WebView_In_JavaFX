ec315f2b9014310f7d717e5e50fb3f2ac31f0bfe
Anna Hoerschgen <ah0959247@otc.edu>
2025-02-06 12:31:16 -0600

    First Commit

---
+++ ModuleTwo.java
@@ -0,0 +1,21 @@
+/*
+ * Name: Anna Hoerschgen
+ * Date: Febuary 6, 2025
+ * Filename: ModuleTwo.java
+ * Assignment: Simple Web Browser in JavaFX
+ */
+
+// Imports
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+public class ModuleTwo {
+    public static void main(String[] args) {
+        //
+    }
+}
